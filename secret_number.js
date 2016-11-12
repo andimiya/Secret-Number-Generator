041f194ec9938f74428935b807c0c144324b56b2
Andrea <takamiya.andrea@gmail.com>
2016-11-12 13:43:55 -1000

    general structure is there, figuring out why tests aren't passing.

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,28 @@
 'use strict';
 module.exports = function() {
-  
+
+
+
+  var secretNumber = (function (){
+
+
+      var secretNumberGenerator = function(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        Math.floor(Math.random()*(max - min + 1)) + min;
+    };
+
+
+    return secretNumberGenerator;
+
+    // secretNumberGenerator(0,100000);
+
+  })();
+
+
+
+// var secretNumber = secretNumberGenerator();
+console.log (secretNumber());
+// console.log(secretNumber());
+
 };
