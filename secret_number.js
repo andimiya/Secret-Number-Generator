f74c1f86d5183b9dc90ca767b49c661560ba1d7b
Andrea <takamiya.andrea@gmail.com>
2016-11-12 14:33:13 -1000

    solution, all tests pass

--- secret_number.js
+++ secret_number.js
@@ -1,28 +1,29 @@
 'use strict';
 module.exports = function() {
+  var randomNumber = Math.random()*1000000;
+  var myNum = function (){
+    return randomNumber;
+  };
+  return myNum;
+};
 
 
 
-  var secretNumber = (function (){
+//   var secretNumber = (function (){
 
-
-      var secretNumberGenerator = function(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        Math.floor(Math.random()*(max - min + 1)) + min;
-    };
-
-
-    return secretNumberGenerator;
-
-    // secretNumberGenerator(0,100000);
-
-  })();
-
-
+//       var secretNumberGenerator = function(min, max) {
+//         min = Math.ceil(min);
+//         max = Math.floor(max);
+//         var secretNumber = (Math.floor(Math.random()*(max - min + 1)) + min);
+//         console.log(secretNumber);
+//         return secretNumberGenerator;
+//         return Math.floor(Math.random()*(max - min + 1)) + min;
+//       };
+//     return secretNumberGenerator;
+//   })();
+// };
 
 // var secretNumber = secretNumberGenerator();
-console.log (secretNumber());
-// console.log(secretNumber());
-
-};
+// var test1 = secretNumber();
+// console.log (secretNumber());
+// // console.log(secretNumber());
